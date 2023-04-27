9078c7ab8179553f49c2e37178b2c6898b7da2a0
safietou <safietou.ndoye@outlook.com>
2023-04-27 17:37:58 +0200

    Ajoutez des fichiers projet.

---
+++ Models/Résultat.cs
@@ -0,0 +1,7 @@
+﻿namespace PROJET.Models
+{
+    public class Résultat
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Models/Sauvegarde.cs
@@ -0,0 +1,10 @@
+﻿namespace PROJET.Models
+{
+    public class Sauvegarde
+    {
+        public int Heure{ get; set; }
+        public int Date { get; set; }
+
+
+    }
+}
