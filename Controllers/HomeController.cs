0aa1929b66c9b026a3240c686551745b7c89556c
safietou <safietou.ndoye@outlook.com>
2023-05-24 19:03:42 +0200

    changes3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -214,7 +214,19 @@ namespace PROJET.Controllers
 
         }
 
-        
+  
+        public IActionResult Comparegraph()
+        {
+            return View();
+
+        }
+
+        public IActionResult Historique()
+        {
+            return View();
+
+        }
+
         [HttpPost]
         public IActionResult Hyperparameter(Hyperparamètres model)
         {
